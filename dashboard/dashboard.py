79ba9a4b5f977a66b2b47829c25deba4ff2c345b
Armands1234 <152397186+Armands1234@users.noreply.github.com>
2023-11-30 08:58:54 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,7 +9,6 @@ def get_total_count_by_hour_df(hour_df):
     hour_count_df =  hour_df.groupby(by="hour").agg({"count_cr": ["sum"]})
     hour_count_df.columns = ["_".join(col).strip() for col in hour_count_df.columns.values]
     return hour_count_df
-print(hour_df.columns)
 
 def count_by_day_df(day_df):
     day_df_count_2011 = day_df.query(str('dteday >= "2011-01-01" and dteday < "2012-12-31"'))
